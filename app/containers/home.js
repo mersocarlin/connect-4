0f78b16cdcda320fb92d5f5a6f318494c2cda2ec
mersocarlin <mersocarlin@gmail.com>
2016-01-04 21:26:06 -0200

    Add board to home container

--- app/containers/home.js
+++ app/containers/home.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Board from '../components/board';
+
+import { playWithRed, playWithYellow } from '../actions/connect4';
+import { RED_TURN, YELLOW_TURN } from '../reducers/connect4';
+
 
 class Home extends Component {
+  onBoardClick (col) {
+    const { connect4 } = this.props;
+
+    switch (connect4.playingNow) {
+      default:
+      case RED_TURN:
+        this.props.dispatch(playWithRed(col));
+        break;
+      case YELLOW_TURN:
+        this.props.dispatch(playWithYellow(col));
+        break;
+    }
+  }
+
   render () {
+    const { connect4 } = this.props;
+
+    console.log('props', connect4);
+
     return (
       <div className="app-page page-home">
-        Home
+        <Board
+          board={connect4.board}
+          onClick={this.onBoardClick.bind(this)}
+        />
       </div>
     );
   }
 }
 
-export default connect((/* state */) => {
+export default connect((state) => {
   return {
-
+    connect4: state.connect4,
   };
 })(Home);
