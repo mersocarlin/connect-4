bce022392ab621ba48cad76f421746aa3de4e618
mersocarlin <mersocarlin@gmail.com>
2016-01-05 21:33:30 -0200

    Add PIXI board to home container

--- app/containers/home.js
+++ app/containers/home.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Board from '../components/board';
+import redIMG from '../img/red.png';
+import yellowIMG from '../img/yellow.png';
+import emptyIMG from '../img/empty.png';
 
-import { playWithRed, playWithYellow } from '../actions/connect4';
+import {
+  newGame,
+  playWithRed,
+  playWithYellow,
+} from '../actions/connect4';
 import { RED_TURN, YELLOW_TURN } from '../reducers/connect4';
+import { BOARD_SIZE, PIECE_SIZE } from '../domain/board';
+
+import '../scripts/pixi.min.js';
+const WebGLRenderer = PIXI.WebGLRenderer;
+const Container = PIXI.Container;
+const size = PIECE_SIZE * BOARD_SIZE;
+const renderer = new WebGLRenderer(size, size);
+const stage = new Container();
 
 
 class Home extends Component {
+
   onBoardClick (col) {
     const { connect4 } = this.props;
 
@@ -22,17 +37,99 @@ class Home extends Component {
     }
   }
 
+  getTextureByValue (type) {
+    let img;
+
+    switch (type) {
+      default:
+      case 0:
+        img = emptyIMG;
+        break;
+      case 1:
+        img = redIMG;
+        break;
+      case 2:
+        img = yellowIMG;
+        break;
+    }
+
+    return new PIXI.Texture.fromImage(img);
+  }
+
+  handleNewGameClick () {
+    this.props.dispatch(newGame());
+  }
+
+  renderPIXIBoard () {
+    const { connect4 } = this.props;
+    const { board } = connect4;
+    const Sprite = PIXI.Sprite;
+
+    for (let row = 0; row < BOARD_SIZE; row++) {
+      const rowPos = (BOARD_SIZE - 1) - row;
+      for (let col = 0; col < BOARD_SIZE; col++) {
+        const texture = this.getTextureByValue(board.getValueAt(row, col));
+        const piece = new Sprite(texture);
+
+        piece.x = col * PIECE_SIZE;
+        piece.y = rowPos * PIECE_SIZE;
+        piece.interactive = true;
+
+        piece.mousedown = (e) => {
+          const { target } = e;
+
+          switch (connect4.playingNow) {
+            default:
+            case RED_TURN:
+              this.props.dispatch(playWithRed(target.col));
+              break;
+            case YELLOW_TURN:
+              this.props.dispatch(playWithYellow(target.col));
+              break;
+          }
+
+          this.renderPIXIBoard();
+        };
+
+        piece.row = row;
+        piece.col = col;
+
+        // Add the rocket to the stage
+        stage.addChild(piece);
+      }
+    }
+  }
+
   render () {
     const { connect4 } = this.props;
 
     console.log('props', connect4);
 
+    // return (
+    //   <div className="app-page page-home">
+    //     <Board
+    //       board={connect4.board}
+    //       onClick={this.onBoardClick.bind(this)}
+    //     />
+    //   </div>
+    // );
+
+    this.renderPIXIBoard(stage);
+
+    if (!document.getElementsByTagName('canvas').length) {
+      document.body.appendChild(renderer.view);
+    }
+
+    const animate = () => {
+      renderer.render(stage);
+      requestAnimationFrame(animate);
+    };
+
+    requestAnimationFrame(animate);
+
     return (
       <div className="app-page page-home">
-        <Board
-          board={connect4.board}
-          onClick={this.onBoardClick.bind(this)}
-        />
+        <a onClick={::this.handleNewGameClick}>New Game</a>
       </div>
     );
   }
